850f9d2eca7a319ba8f0c421d2104920bdd7bb7d
HARSHHARSANI <harshharsani007@gmail.com>
2023-11-02 23:46:25 +0530

    user Controller Validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 import userModel from "../models/userModel.js";
+import { validateEmail, validatelength } from "../helpers/validation.js";
+import bcrypt from "bcrypt";
 
 export const register = async (req, res) => {
   try {
@@ -14,12 +16,52 @@ export const register = async (req, res) => {
       gender,
     } = req.body;
 
+    if (!validateEmail(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Email Address",
+      });
+    }
+
+    if (!validatelength(first_name, 2, 30)) {
+      return res.status(400).json({
+        success: false,
+        message: "first name Must be Between 2 and 30",
+      });
+    }
+
+    if (!validatelength(last_name, 2, 30)) {
+      return res.status(400).json({
+        success: false,
+        message: "Last Name Must be Between 2 and 30",
+      });
+    }
+
+    if (!validatelength(password, 6, 15)) {
+      return res.status(400).json({
+        success: false,
+        message: "password Must be Between 6 and 15",
+      });
+    }
+
+    const check = await userModel.findOne({ email });
+    if (check) {
+      return res.status(400).json({
+        success: false,
+        message:
+          "This Email Address Already Exist Try With Another Email Address",
+      });
+    }
+
+    const cryptedPassword = await bcrypt.hash(password, 3);
+    console.log(cryptedPassword);
+
     const user = await new userModel({
       first_name,
       last_name,
       username,
       email,
-      password,
+      password: cryptedPassword,
       bYear,
       bMonth,
       bDay,
--- backend/helpers/validation.js
+++ backend/helpers/validation.js
@@ -1,1 +1,27 @@
-export const validateEmail = () => {};
+import userModel from "../models/userModel";
+
+export const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+};
+
+export const validatelength = (text, min, max) => {
+  if (text.length > max || text.length < min) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+export const validateUsername = async (username) => {
+  let a = false;
+  do {
+    let check = await userModel.findOne({ username });
+    if (check) {
+      ///change the username
+    } else {
+      
+    }
+  } while (a);
+};
