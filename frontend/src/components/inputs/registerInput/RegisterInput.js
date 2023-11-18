d6c1fd65290d740e3effb214699aaf44709c49fe
HARSHHARSANI <harshharsani007@gmail.com>
2023-11-18 11:09:00 +0530

    fixed few styling

--- frontend/src/components/inputs/registerInput/RegisterInput.js
+++ frontend/src/components/inputs/registerInput/RegisterInput.js
@@ -15,12 +15,23 @@ const RegisterInput = ({ placeholder, bottom, ...props }) => {
   });
   // console.log(desktopView);
 
+  const test1 = view3 && field.name === "first_name";
+  const test2 = view3 && field.name === "last_name";
+
   return (
     <div className="input_wrap register_input_wrap">
       <input
         className={meta.touched && meta.error ? "input_error_border" : ""}
-        style={{width:`${view1 &&  }`}}
-        type={props.type || "text"}
+        style={{
+          width: `${
+            view1 && (field.name === "first_name" || field.name === "last_name")
+              ? "100%"
+              : view1 && (field.name === "email" || field.name === "password")
+              ? "370px"
+              : "300px"
+          }`,
+        }}
+        type={props.type}
         name={field.name}
         placeholder={placeholder}
         {...field}
@@ -28,16 +39,21 @@ const RegisterInput = ({ placeholder, bottom, ...props }) => {
       />
       {meta.touched && meta.error && (
         <div
-          className={
-            view2 ? "input_error input_error_desktop" : "input_error"
-          }
-          style={{ transform: "translateY(1px)" }}
+          className={view3 ? "input_error input_error_desktop" : "input_error"}
+          style={{
+            transform: "translateY(2px)",
+            left: `${test1 ? "-107%" : test2 ? "107%" : ""}`,
+          }}
         >
           {meta.touched && meta.error && <ErrorMessage name={field.name} />}
           {meta.touched && meta.error && (
             <div
               className={
-                view2 ? "error_arrow_left" : "error_arrow_bottom"
+                view3 && field.name !== "last_name"
+                  ? "error_arrow_left"
+                  : view3 && field.name === "last_name"
+                  ? "error_arrow_right"
+                  : !view3 && "error_arrow_bottom"
               }
             ></div>
           )}
